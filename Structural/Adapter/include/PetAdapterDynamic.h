a0d55963dcb944e743b9c108f952102fd1dc4d56
Aheguy4ik <matveiluzkov@inbox.ru>
2025-06-01 15:09:59 +0300

    FIX

--- Structural/Adapter/include/PetAdapterDynamic.h
+++ Structural/Adapter/include/PetAdapterDynamic.h
@@ -2,28 +2,14 @@
 #include "Pet.h"
 #include "Monster.h"
 #include <memory>
-#include <algorithm>
 
 class PetAdapterDynamic : public Pet {
 public:
-    explicit PetAdapterDynamic(std::unique_ptr<Monster>&& monster)
-        : monster_(std::move(monster)) {}
-
-    std::unique_ptr<Monster> ChangeMonster(std::unique_ptr<Monster>&& monster) {
-        auto old = std::move(monster_);
-        monster_ = std::move(monster);
-        return old;
-    }
-
-    std::string Name() const override { return monster_->Name(); }
-
-    int CutenessAbility() const override {
-        return std::max(0, monster_->DefenseAbility() - 2 * monster_->AttackAbility());
-    }
-
-    int BattleBonus() const override {
-        return monster_->AttackAbility() + monster_->DefenseAbility() / 2;
-    }
+    explicit PetAdapterDynamic(std::unique_ptr<Monster>&& monster);
+    std::unique_ptr<Monster> ChangeMonster(std::unique_ptr<Monster>&& monster);
+    std::string Name() const override;
+    int CutenessAbility() const override;
+    int BattleBonus() const override;
 
 private:
     std::unique_ptr<Monster> monster_;
