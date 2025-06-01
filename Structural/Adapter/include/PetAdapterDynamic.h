f0402cf6756376adb32acb58e16e414a9a03479f
Aheguy4ik <matveiluzkov@inbox.ru>
2025-06-01 15:06:23 +0300

    FIX

--- Structural/Adapter/include/PetAdapterDynamic.h
+++ Structural/Adapter/include/PetAdapterDynamic.h
@@ -2,16 +2,28 @@
 #include "Pet.h"
 #include "Monster.h"
 #include <memory>
+#include <algorithm>
 
 class PetAdapterDynamic : public Pet {
 public:
-    explicit PetAdapterDynamic(std::unique_ptr<Monster>&& monster);
+    explicit PetAdapterDynamic(std::unique_ptr<Monster>&& monster)
+        : monster_(std::move(monster)) {}
 
-    std::unique_ptr<Monster> ChangeMonster(std::unique_ptr<Monster>&& monster);
+    std::unique_ptr<Monster> ChangeMonster(std::unique_ptr<Monster>&& monster) {
+        auto old = std::move(monster_);
+        monster_ = std::move(monster);
+        return old;
+    }
 
-    std::string Name() const override;
-    int CutenessAbility() const override;
-    int BattleBonus() const override;
+    std::string Name() const override { return monster_->Name(); }
+
+    int CutenessAbility() const override {
+        return std::max(0, monster_->DefenseAbility() - 2 * monster_->AttackAbility());
+    }
+
+    int BattleBonus() const override {
+        return monster_->AttackAbility() + monster_->DefenseAbility() / 2;
+    }
 
 private:
     std::unique_ptr<Monster> monster_;
--- Structural/Adapter/include/PetAdapterStatic.h
+++ Structural/Adapter/include/PetAdapterStatic.h
@@ -2,13 +2,18 @@
 #include "Monster.h"
 #include "Pet.h"
 #include <type_traits>
+#include <algorithm> // for std::max
 
 template <typename MonsterType, typename = std::enable_if_t<std::is_base_of<Monster, MonsterType>::value>>
 class PetAdapterStatic : public Pet {
 public:
     std::string Name() const override { return monster_.Name(); }
-    int CutenessAbility() const override { return monster_.DefenseAbility() - 2 * monster_.AttackAbility(); }
-    int BattleBonus() const override { return monster_.AttackAbility() + monster_.DefenseAbility() / 2; }
+    int CutenessAbility() const override {
+        return std::max(0, monster_.DefenseAbility() - 2 * monster_.AttackAbility());
+    }
+    int BattleBonus() const override {
+        return monster_.AttackAbility() + monster_.DefenseAbility() / 2;
+    }
 
 private:
     MonsterType monster_;
